aa7b730962e72d9bac198fe141b0e482dd57c9a4
Leonardo1903 <fmartins.leonardo@gmail.com>
2021-12-01 14:41:24 -0300

    endpoint de achar pais por Id

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,27 @@ app.get("/paises", async (req: Request, res: Response) => {
         res.status(500).send({ message: error.message })
     }
 })
+
+app.get("/paises/:id", (req: Request, res: Response) => {
+
+    try {
+
+        if (isNaN(Number(req.params.id))) {
+            throw new Error("Deve ser um numero")
+        }
+
+        const id = Number(req.params.id);
+        const result = countries.find((country => country.id === id));
+
+        if (!result) {
+            throw new Error("Não há pais com esse ID")
+        }
+
+        res.
+            status(200)
+            .send(result)
+
+    } catch (erro: any) {
+        res.status(400).send({ message: erro.message })
+    }
+})
